8a5a8a7c29545c6cda2b83260fe0f8d9a98de67f
ArianeDlns <38358228+ArianeDlns@users.noreply.github.com>
2021-10-01 14:27:29 +0200

    TP1 1.1

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Ariane Dalens
 
 ### TP 1.
+
+[Sujet TP.1](https://wdi.centralesupelec.fr/3IF1020/SystExercice1)
--- ps_tp1/server_1.c
+++ ps_tp1/server_1.c
@@ -5,6 +5,7 @@
  * server_1.c
  */
 
+
 // for printf()
 #include <stdio.h>
 // For rand(), srand(), sleep(), EXIT_SUCCESS
@@ -16,7 +17,19 @@
 
 int main()
 {
-    // TODO
+    printf("Starting program \n");
+
+    while (true){
+          int pid(getpid());
+          int fatherpid(getppid());
+          int random_nub(rand() % 100);
+
+          printf("pid: %d \n" , (pid));
+          printf("father pid: %d \n" , (fatherpid));
+          printf("randint: %d \n" , (random_nub));
+
+          sleep(1);
+    }
         
     return EXIT_SUCCESS;
 }
