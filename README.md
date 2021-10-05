e2e7ecd0ec448afd2b1b83d3bd0c7e40bfb789d7
ArianeDlns <38358228+ArianeDlns@users.noreply.github.com>
2021-10-05 15:02:28 +0200

    [clp_tp1] adding sort_tab_1()

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 
 ### TP 1.
 
-[Sujet TP.1 Programmation système](https://wdi.centralesupelec.fr/3IF1020/SystExercice1)
-[Sujet TP.1 Concepts des langages de programmation](https://wdi.centralesupelec.fr/3IF1020/ProgExercice1)
+:desktop_computer: [Sujet TP.1 Programmation système](https://wdi.centralesupelec.fr/3IF1020/SystExercice1)  
+
+:keyboard: [Sujet TP.1 Concepts des langages de programmation](https://wdi.centralesupelec.fr/3IF1020/ProgExercice1)
 
 ### TP 2.
--- clp_tp1/tris.cpp
+++ clp_tp1/tris.cpp
@@ -15,7 +15,7 @@
 
 using namespace std;
 
-void print_tab(std::vector<int> tab){
+void print_tab(const std::vector<int> &tab){
     std::cout << "[ ";
     for (int i(0); i < tab.size(); i++){
         std::cout << tab[i] << " ";
@@ -45,13 +45,49 @@ void test_12() {
 }
 
 void sort_tab_1(std::vector<int> &tab){
+    for (int i{0}; i < tab.size() - 1; i++) {
+        int min_index{i};
+        int min_value{tab[i]};
+        for (int j{i+1}; j < tab.size(); j++) {
+            if (tab[j] < min_value) {
+                min_value = tab[j];
+                min_index = j;
+            }
+        }
+        if (min_index != i) {
+            std::swap(tab[i], tab[min_index]);
+            }
+    }
+}
+
+void test_13(){
+    std::cout << "*** test_13 ***" << std::endl;
+    std::vector<int> tab(10);
+    random_tab(tab);
+    print_tab(tab);
+    std::cout << " >> SORTED LIST >> ";
+    sort_tab_1(tab);
+    print_tab(tab);
+    std::cout << std::endl;
+}
+
+bool less(int a, int b){
+}
+
+bool greater(int a, int b){
+}
+
+void sort_tab_2(std::vector<int> &tab, bool(*comp)(int,int)){
 }
 
 int main(){
     //permet de changer l'aléatoire en dépendant de l'horloge 
     std::srand( std::time( nullptr ));
+
+    //test
     test_11();
     test_12();
+    test_13();
     return 0;
 }
 
