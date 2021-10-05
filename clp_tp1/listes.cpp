d4af1453c70e825de33309f80eb81dbaf9333e15
Ariane Dlns <ariane.dalens@student.ecp.fr>
2021-10-05 15:55:54 +0200

    [clp_tp1] adding print_list

--- clp_tp1/listes.cpp
+++ clp_tp1/listes.cpp
@@ -13,11 +13,35 @@
 #include <cstdlib>
 #include <ctime>
 
+std::forward_list<int> random_list(int nb) {
+    std::forward_list<int> list;
+    for (int i = 0; i < nb; i++) {
+        int random_number{std::rand()%100};
+        list.push_front(random_number);
+    }
+    return list;
+}
 
+void print_list(std::forward_list<int> list) {
+    std::cout << "( ";
+    for( int i : list ) std::cout << i << " ";
+    std::cout << ")" << std::endl;
+}
+
+void test_21() {
+    std::cout << "*** test_21 ***" << std::endl;
+    auto list = random_list(10);
+    print_list(list);
+    std::cout << std::endl;
+}
 
 int main()
 {
+    //permet de changer l'aléatoire en dépendant de l'horloge 
     std::srand( std::time( nullptr ));
+
+    //tests
+    test_21();
 
     return 0;
 }
